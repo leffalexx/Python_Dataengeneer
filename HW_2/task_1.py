4663ee8211aae09038ff936f021e532be67e82d9
Aleksei <leffalexx@gmail.com>
2023-09-23 22:38:27 +0400

    second done

---
+++ HW_2/task_1.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+HEX_LETTER_OFFSET = 87 # для заглавных букв константа 55
+
+def decimal_to_hex(dec_num):
+    hex_num = ''
+    while dec_num > 0:
+        hex_digit = dec_num % 16
+        if hex_digit < 10:
+            hex_digit = str(hex_digit)
+        else:
+            hex_digit = chr(hex_digit + HEX_LETTER_OFFSET)
+        hex_num = hex_digit + hex_num
+        dec_num = dec_num // 16
+
+    return hex_num
+
+
+decimal_num = int(input('Введите целое число: '))
+print(decimal_to_hex(decimal_num))
+print(hex(decimal_num))
---
+++ HW_2/task_2.py
@@ -0,0 +1,37 @@
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions
+
+import fractions
+
+
+def fraction_calc(fraction1, fraction2):
+    a, b = fraction1.split('/')
+    c, d = fraction2.split('/')
+    a, b, c, d = int(a), int(b), int(c), int(d)
+
+    sum_numerator = a*d + b*c
+    sum_denominator = b*d
+    sum_fraction = f'{sum_numerator}/{sum_denominator}'
+
+    mult_numerator = a*c
+    mult_denominator = b*d
+    mult_fraction = f'{mult_numerator}/{mult_denominator}'
+
+    return sum_fraction, mult_fraction
+
+
+fraction1 = input('Введите первую дробь в виде a/b: ')
+fraction2 = input('Введите вторую дробь в виде c/d: ')
+
+sum_fraction, mult_fraction = fraction_calc(fraction1, fraction2)
+
+print(f'Сумма: {sum_fraction}')
+print(f'Произведение: {mult_fraction}')
+
+print('Проверка модулем Fractions: ')
+
+x = fractions.Fraction(1, 7)
+y = fractions.Fraction(5, 9)
+
+print(f'Сумма: {x+y}')
+print(f'Произведение: {x*y}')
